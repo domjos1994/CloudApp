a2f3d1858effbe6be134dfa0aa6d203b1a9d0524
domjos1994 <developing@domjos.de>
2024-04-12 13:45:17 +0200

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "de.domjos.cloudapp"
         minSdk = rootProject.extra["sdk_min"] as Int
         targetSdk = rootProject.extra["sdk_compile"] as Int
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
