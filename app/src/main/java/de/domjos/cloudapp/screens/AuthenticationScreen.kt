4cd235b2be69d6e882c7ae06b95cbc4fd02662df
domjos1994 <developing@domjos.de>
2024-05-03 10:58:56 +0200

    solve bug 0001002

--- app/src/main/java/de/domjos/cloudapp/screens/AuthenticationScreen.kt
+++ app/src/main/java/de/domjos/cloudapp/screens/AuthenticationScreen.kt
@@ -354,7 +354,7 @@ private fun EditDialog(
                             )
 
                             onConnectionCheck(auth) {
-                                isConnectionValid = it!=null
+                                isConnectionValid = it!=null && isValidTitle && isValidUrl && isValidDescription && isConnectionValid
                                 color = if(isConnectionValid) Color.Green else Color.Red
                             }
                         }, colors = ButtonDefaults.buttonColors(containerColor = color)
