3b3815577c539166ca6a4b4609b73366f328733d
domjos1994 <developing@domjos.de>
2024-06-25 21:41:54 +0200

    update tests and copyright

--- REST/src/main/java/de/domjos/cloudapp2/rest/model/user/OCS.kt
+++ REST/src/main/java/de/domjos/cloudapp2/rest/model/user/OCS.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Dominic Joas
+ * This file is part of the CloudApp-Project and licensed under the
+ * General Public License V3.
+ */
+
 package de.domjos.cloudapp2.rest.model.user
 
 import de.domjos.cloudapp2.rest.model.ocs.Meta
