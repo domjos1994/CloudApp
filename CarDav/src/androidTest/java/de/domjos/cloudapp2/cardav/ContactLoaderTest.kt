378c3c8ba12a8cb102b2167827a416b7ccffb1ba
domjos1994 <developing@domjos.de>
2024-06-26 18:44:19 +0200

    update tests in rest

--- CarDav/src/androidTest/java/de/domjos/cloudapp2/cardav/ContactLoaderTest.kt
+++ CarDav/src/androidTest/java/de/domjos/cloudapp2/cardav/ContactLoaderTest.kt
@@ -1,6 +1,12 @@
+/*
+ * Copyright (c) 2024 Dominic Joas
+ * This file is part of the CloudApp-Project and licensed under the
+ * General Public License V3.
+ */
+
 package de.domjos.cloudapp2.cardav
 
-import de.domjos.cloudapp2.database.model.Authentication
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import de.domjos.cloudapp2.database.model.contacts.Address
 import de.domjos.cloudapp2.database.model.contacts.Contact
 import de.domjos.cloudapp2.database.model.contacts.Email
@@ -8,6 +14,7 @@ import de.domjos.cloudapp2.database.model.contacts.Phone
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 import java.util.Date
 import java.util.LinkedList
 import java.util.UUID
@@ -17,25 +24,16 @@ import java.util.UUID
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ContactLoaderTest {
+@RunWith(AndroidJUnit4::class)
+class ContactLoaderTest : BasicTest() {
     @Test
     fun testLoadAddressBooks() {
-        val authentication = Authentication(
-            0L, "cloud", "https://cloud.dojodev.de", "domjos",
-            "wRsesbg2M9D7CW3Uc68E", true, null, null
-        )
-
         val loader = ContactLoader(authentication)
         assertNotEquals(0, loader.getAddressBooks().size)
     }
 
     @Test
     fun testLoadAddressBook() {
-        val authentication = Authentication(
-            0L, "cloud", "https://cloud.dojodev.de", "domjos",
-            "wRsesbg2M9D7CW3Uc68E", true, null, null
-        )
-
         val loader = ContactLoader(authentication)
         val addressBooks = loader.getAddressBooks()
         assertNotEquals(0, addressBooks.size)
@@ -46,27 +44,22 @@ class ContactLoaderTest {
 
     @Test
     fun testInsertContacts() {
-        val authentication = Authentication(
-            0L, "cloud", "https://cloud.dojodev.de", "domjos",
-            "wRsesbg2M9D7CW3Uc68E", true, null, null
-        )
-
         val loader = ContactLoader(authentication)
         val addressBooks = loader.getAddressBooks()
         assertNotEquals(0, addressBooks.size)
         val uid = UUID.randomUUID().toString()
 
-        val contact = Contact(
-            uid,
-            "", "", "Joas", "Dominic2", "",
-            Date(), "", null, addressBooks[0]
+        val contact = Contact(0L, uid,
+            "", "", "Joas", "Dominic2", "", "",
+            Date(), "", null, addressBooks[0], "",
+            0L, 0L, 0L
         )
         contact.categories = LinkedList<String>()
         contact.addresses = LinkedList<Address>()
         contact.phoneNumbers = LinkedList<Phone>()
         contact.emailAddresses = LinkedList<Email>()
 
-        loader.insertContact(addressBooks[1], contact)
+        loader.insertContact(contact)
         var contacts = loader.loadAddressBook(addressBooks[1])
 
         var id = ""
@@ -77,7 +70,7 @@ class ContactLoaderTest {
         }
         assertNotNull(id)
 
-        loader.deleteContact(addressBooks[1], contact)
+        loader.deleteContact(contact)
         contacts = loader.loadAddressBook(addressBooks[1])
 
         id = ""
@@ -91,26 +84,21 @@ class ContactLoaderTest {
 
     @Test
     fun testUpdateContact() {
-        val authentication = Authentication(
-            0L, "cloud", "https://cloud.dojodev.de", "domjos",
-            "wRsesbg2M9D7CW3Uc68E", true, null, null
-        )
-
         val loader = ContactLoader(authentication)
         val addressBooks = loader.getAddressBooks()
         assertNotEquals(0, addressBooks.size)
         val uid = UUID.randomUUID().toString()
 
         val contact = Contact(
-            uid,
-            "", "", "Joas", "Dominic2", "",
-            Date(), "", null, addressBooks[0]
+            0L, uid, "", "", "","Joas", "Dominic2", "",
+            Date(), "", null, addressBooks[0], "",
+            0L, 0L, 0L
         )
         contact.categories = LinkedList<String>()
         contact.addresses = LinkedList<Address>()
         contact.phoneNumbers = LinkedList<Phone>()
         contact.emailAddresses = LinkedList<Email>()
-        loader.insertContact(addressBooks[1], contact)
+        loader.insertContact(contact)
         var contacts = loader.loadAddressBook(addressBooks[1])
 
         var c: Contact? = null
@@ -122,7 +110,7 @@ class ContactLoaderTest {
         assertNotNull(c)
 
         c?.givenName = "Dominic3"
-        loader.insertContact(addressBooks[1], c!!)
+        loader.insertContact(c!!)
 
         contacts = loader.loadAddressBook(addressBooks[1])
 
@@ -134,7 +122,7 @@ class ContactLoaderTest {
         }
         assertNotNull(c)
 
-        loader.deleteContact(addressBooks[1], contact)
+        loader.deleteContact(contact)
         contacts = loader.loadAddressBook(addressBooks[1])
 
         c = null
