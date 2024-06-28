d0857dc0073189463f47f9f49487464065ee0499
domjos1994 <developing@domjos.de>
2024-06-28 11:42:35 +0200

    update tests

--- CalendarFeature/src/androidTest/java/de/domjos/cloudapp2/features/calendars/ExampleInstrumentedTest.kt
+++ CalendarFeature/src/androidTest/java/de/domjos/cloudapp2/features/calendars/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("de.domjos.cloudapp.features.calendarfeature.test", appContext.packageName)
+        assertEquals("de.domjos.cloudapp2.features.calendarfeature.test", appContext.packageName)
     }
 }
