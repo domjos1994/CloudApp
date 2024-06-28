c1a61c5124b303c689ae8a6315230fcdb21ad87b
domjos1994 <developing@domjos.de>
2024-06-28 11:31:28 +0200

    disable tests

--- CarDav/src/androidTest/java/de/domjos/cloudapp2/cardav/ContactLoaderTest.kt
+++ CarDav/src/androidTest/java/de/domjos/cloudapp2/cardav/ContactLoaderTest.kt
@@ -24,7 +24,7 @@ import java.util.UUID
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+//@RunWith(AndroidJUnit4::class)
 class ContactLoaderTest : BasicTest() {
     //@Test
     fun testLoadAddressBooks() {
