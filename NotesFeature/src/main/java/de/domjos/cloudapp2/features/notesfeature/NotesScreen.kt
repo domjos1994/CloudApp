82a712d8517c586e18f15f6ca0f80ff0192a59b8
domjos1994 <developing@domjos.de>
2024-10-08 15:06:49 +0200

    solve bug 0001143

--- NotesFeature/src/main/java/de/domjos/cloudapp2/features/notesfeature/NotesScreen.kt
+++ NotesFeature/src/main/java/de/domjos/cloudapp2/features/notesfeature/NotesScreen.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -391,7 +393,9 @@ fun NotesDialog(
                     .border(1.dp, MaterialTheme.colorScheme.primaryContainer)) {
                     Markdown(
                         content = content,
-                        modifier = Modifier.padding(2.dp)
+                        modifier =
+                            Modifier.padding(2.dp).height(250.dp)
+                                .verticalScroll(rememberScrollState())
                     )
                 }
                 Row(
@@ -500,7 +504,12 @@ fun NotesBottomSheet(note: Note?, setShowSheet: (Boolean) -> Unit) {
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center) {
 
-                    Markdown(note.content)
+                    Markdown(
+                        note.content,
+                        modifier =
+                            Modifier.padding(2.dp).height(250.dp)
+                            .verticalScroll(rememberScrollState())
+                    )
                 }
             }
         }
